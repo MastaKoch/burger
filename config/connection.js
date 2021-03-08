c301eb952daf5a5e05f3bfca488410102d0a7926
MastaKoch <alexkoch1996@gmail.com>
2021-03-08 16:30:44 -0500

    creating connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+  connection=mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password123",
   database: "burgers_db"
 });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
@@ -16,6 +22,8 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
